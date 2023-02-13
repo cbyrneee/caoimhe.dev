66ce118660de42aa591f8c33a900233cea0b5691
Caoimhe <caoimhebyrne06@gmail.com>
2023-02-13 23:59:47 +0000

    Don't use ISR or caching

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,32 +1,10 @@
 import { LastClient } from "@musicorum/lastfm";
 import { LASTFM_API_KEY, LASTFM_USERNAME } from "$env/static/private";
 import { getCurrentTrack } from "$lib/lastfm";
-import type MusicInformation from "$types/MusicInformation";
-import { Cached } from "$lib/cache";
 
 const client = new LastClient(LASTFM_API_KEY);
-const cache = new Cached<MusicInformation | undefined>(60);
-
-export const config = {
-	isr: {
-		expiration: 60,
-		group: 1,
-	},
-};
 
 export const load = async () => {
-	if (!cache.expired()) {
-		return { musicInformation: cache.valueIfNotExpired() };
-	}
-
-	try {
-		const track = await getCurrentTrack(client, LASTFM_USERNAME);
-		cache.set(track);
-
-		return { musicInformation: track };
-	} catch (e) {
-		// Ignored
-		console.error("Failed to fetch last.fm track information!", e);
-		cache.set(undefined);
-	}
+	const track = await getCurrentTrack(client, LASTFM_USERNAME);
+	return { musicInformation: track };
 };
