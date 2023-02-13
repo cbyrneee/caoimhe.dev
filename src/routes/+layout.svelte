60adba2b898d7641583c14393a629ddc5abae8b5
Caoimhe <caoimhebyrne06@gmail.com>
2023-02-13 03:47:09 +0000

    Add page fade-in animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
+	import { onMount } from "svelte";
+	import { fade } from "svelte/transition";
 	import "../app.css";
+
+	$: ready = false;
+	onMount(() => (ready = true));
 </script>
 
 <svelte:head>
@@ -8,6 +13,10 @@
 
 <div class="min-w-screen flex min-h-screen">
 	<div class="mx-auto w-full p-6 xl:mx-0 xl:px-64">
-		<slot />
+		{#if ready}
+			<div in:fade={{ duration: 250 }}>
+				<slot />
+			</div>
+		{/if}
 	</div>
 </div>
