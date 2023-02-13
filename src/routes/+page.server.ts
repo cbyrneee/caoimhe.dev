32a2ea0fd3b91f839f85b145207cdd2d98cb5cf5
Caoimhe <caoimhebyrne06@gmail.com>
2023-02-13 17:52:52 +0000

    Catch errors in `+page.server.ts`

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,17 @@
 import { LastClient } from "@musicorum/lastfm";
 import { LASTFM_API_KEY, LASTFM_USERNAME } from "$env/static/private";
 import { getCurrentTrack } from "$lib/lastfm";
+import type MusicInformation from "$types/MusicInformation";
 
 const client = new LastClient(LASTFM_API_KEY);
 
 export const load = async () => {
-	const information = await getCurrentTrack(client, LASTFM_USERNAME);
-	if (!information) {
-		return { musicInformation: null };
+	let information: MusicInformation | null = null;
+	try {
+		information = await getCurrentTrack(client, LASTFM_USERNAME);
+	} catch (e) {
+		// Ignored
+		console.error("Failed to fetch last.fm track information!", e);
 	}
 
 	return { musicInformation: information };
