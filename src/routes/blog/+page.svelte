9103fea7a2b1b93e4b72d2c83a134760276e3dc6
Caoimhe <caoimhebyrne06@gmail.com>
2023-02-13 23:12:01 +0000

    Add basic header to `/blog`

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Card, Link } from "$components/base";
+</script>
+
+<Card class="justify-between bg-gradient-to-br from-red-200/30 via-red-300/30 to-yellow-200/30">
+	<div class="flex flex-col gap-2">
+		<h1 class="text-title">Blog</h1>
+		<h2 class="text-subtitle">
+			This is the place where you'll find all my cool write-ups on things that I find
+			interesting.
+		</h2>
+	</div>
+</Card>
