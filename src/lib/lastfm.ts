d3ad713a07094185ff07eb54708f1559ce74ff9f
Caoimhe <caoimhebyrne06@gmail.com>
2023-02-13 23:11:54 +0000

    Use caching for last.fm information

--- src/lib/lastfm.ts
+++ src/lib/lastfm.ts
@@ -4,12 +4,12 @@ import type { LastClient } from "@musicorum/lastfm";
 export async function getCurrentTrack(
 	client: LastClient,
 	username: string,
-): Promise<MusicInformation | null> {
+): Promise<MusicInformation | undefined> {
 	const recentTracks = await client.user.getRecentTracks(username);
 	const track = recentTracks.tracks.at(0);
 
 	if (!track || !track.nowPlaying) {
-		return null;
+		return undefined;
 	}
 
 	return {
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,17 +2,24 @@ import { LastClient } from "@musicorum/lastfm";
 import { LASTFM_API_KEY, LASTFM_USERNAME } from "$env/static/private";
 import { getCurrentTrack } from "$lib/lastfm";
 import type MusicInformation from "$types/MusicInformation";
+import { Cached } from "$lib/cache";
 
 const client = new LastClient(LASTFM_API_KEY);
+const cache = new Cached<MusicInformation | undefined>(60);
 
 export const load = async () => {
-	let information: MusicInformation | null = null;
+	if (!cache.expired()) {
+		return { musicInformation: cache.valueIfNotExpired() };
+	}
+
 	try {
-		information = await getCurrentTrack(client, LASTFM_USERNAME);
+		const track = await getCurrentTrack(client, LASTFM_USERNAME);
+		cache.set(track);
+
+		return { musicInformation: track };
 	} catch (e) {
 		// Ignored
 		console.error("Failed to fetch last.fm track information!", e);
+		cache.set(undefined);
 	}
-
-	return { musicInformation: information };
 };
