a331aa128f15c2f2d7fe0748a2636adba5f4671c
Caoimhe <caoimhebyrne06@gmail.com>
2023-02-23 23:16:35 +0000

    Use webp for images

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -10,14 +10,14 @@ const projects: Project[] = [
 		title: "Compact Chat",
 		description:
 			"A mod for Minecraft which compacts messages which have been sent more than once.",
-		image: "/images/compact-chat.png",
+		image: "/images/compact-chat.webp",
 		link: "https://modrinth.com/mod/compact-chat",
 	},
 	{
 		title: "Scroll Director",
 		description:
 			"An application for macOS which toggles the 'Natural Scrolling' setting whenever an external mouse is connected/disconnected.",
-		image: "/images/scroll-director.png",
+		image: "/images/scroll-director.webp",
 		link: "https://github.com/cbyrneee/scroll-director",
 	},
 	{
@@ -25,7 +25,7 @@ const projects: Project[] = [
 		description:
 			'A better <code class="code-block">@Inject</code> annotation for Mixin. It provides better local capture, completely optional arguments, and more.',
 
-		image: "/images/betterinject.png",
+		image: "/images/betterinject.webp",
 		link: "https://github.com/cbyrneee/BetterInject",
 	},
 ];
